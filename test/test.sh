721d9b68483e993e3d2eb6ca018008d6f11e0999
Hassan-Tarek <hassanelashry415@gmail.com>
2025-12-27 13:34:30 +0000

    Fix error in test.sh

--- test/test.sh
+++ test/test.sh
@@ -4,16 +4,11 @@ EXPECTED="Hello, Hassan!"
 
 OUTPUT=$(node -e "console.log(require('./src/app')('Hassan'))")
 
-if [ "$OUTPUT" == "$EXPECTED" ] then
-
-echo "✅ Test passed!"
-
-exit 0
-
+if [ "$OUTPUT" == "$EXPECTED" ] 
+then
+    echo "✅ Test passed!"
+    exit 0
 else 
-
-echo "❌ Test failed! Expected '$EXPECTED' but got '$OUTPUT'"
-
-exit 1
-
+    echo "❌ Test failed! Expected '$EXPECTED' but got '$OUTPUT'"
+    exit 1
 fi
